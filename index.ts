3149a5eaaf498ed96baf5078ed0a9121ff7f9aeb
unknown <syedawardah3@gmail.com>
2024-05-18 00:29:31 +0500

    work complete

--- index.ts
+++ index.ts
@@ -1,47 +1,50 @@
 #! /usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+console.log(chalk.underline.bold.blueBright.bgWhiteBright('\t\t********************WELCOME TO WARDAH CURRENCY EXCHANGE**********************************'))
 
-import inquirer from "inquirer"
-let usdRate : number = 277.90;
-let gbpRate :  number = 351.01;
-let eurRate : number = 301.12;
-let confirmExit;
-do {
- confirmExit = await inquirer.prompt(
-{
-    type: "confirm",
-    name: "exit",
-    message: "DO you want to convert PKR Amount?"
+
+let conversion = {
+    "PKR": {
+        "PKR": 1,
+        "USD": 0.0036,
+        "SR": 0.013,
+    }, "USD": {
+        "USD": 1,
+        "PKR": 278.98,
+        "SR": 3.75,
+    }, "SR": {
+        "USD": 0.27,
+        "PKR": 74.39,
+        "SR": 1,
+    },
+};
+
+const answers: {
+    from: "PKR" | "USD" | "SR",
+    to: "PKR" | "USD" | "SR",
+    amount: number,
+} = await inquirer.prompt([{
+    type: "list",
+    name: "from",
+    message:chalk.redBright.bold ("Select your Currency"),
+    choices: ["PKR", "USD", "SR"],
+}, {
+    type: "list",
+    name: "to",
+    message: chalk.yellowBright.bold("Select Conversion Currency"),
+    choices: ["PKR", "USD", "SR"],
+}, {
+    type: "number",
+    name: "amount",
+    message: chalk.blueBright.bold("Enter amount you want to convert"),
+},
+])
+
+const { from, to, amount } = answers;
+if (from && to && amount) {
+    const result = conversion[from][to] * amount;
+    console.log(chalk.underline.overline.bold.greenBright(`\t\tYour Conversion Amount ${chalk.bgMagentaBright(from)} is converted in ${chalk.bgMagentaBright(to)} and Amount is: ${chalk.bgMagentaBright(result)} in ${chalk.bgMagentaBright(to)} `))
+} else {
+    console.log(chalk.redBright.bold("\t\tPlease enter valid input"));
 }
-);
-if (!confirmExit.exit) {
-    break;
-}
-let currencyAmount = await inquirer.prompt(
-    [
-        {
-            name: "currency",
-            type: "list",
-            choices: [`US Dollar`, `Pound`, `Euro`],
-            message: "please select any currency:"
-            
-        },
-        {
-            name: "amount",
-            type: "number",
-            message: "please enter PKR Amount to convert: "
-        }
-    ]
-    );
-    if (currencyAmount.currency == "US Dollar")
-    {
-        console.log(`Here is your converted US Dollars : $${currencyAmount.amount / usdRate}`);
-    }
-    else if (currencyAmount.currency == "Pound")
-    {
-        console.log(`Here is your converted US Britian Pounds : £${currencyAmount.amount / gbpRate}`);
-    }
-    else
-    {
-        console.log(`Here is your converted Euros : €${currencyAmount.amount / eurRate}`);
-    }
-} while(confirmExit.exit);
